c5a0b143ca9aa7a848de74696723d8befb50d925
QuardNard <tullochyorkcity@gmail.com>
2022-08-12 20:36:58 -0400

    mongoDB connected

--- server/dist/app.js
+++ server/dist/app.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const db_1 = require("./database/db");
 const errorMiddleware_1 = require("./middleware/errorMiddleware");
 const config_1 = require("./utils/config");
+(0, db_1.connectDB)();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use('/api/projects', require("./routes/projectRoutes"));
--- server/dist/utils/config.js
+++ server/dist/utils/config.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.NODE_ENV = exports.PORT = void 0;
+exports.MONGO_URI = exports.NODE_ENV = exports.PORT = void 0;
 require('dotenv').config();
 exports.PORT = process.env.PORT || 5000;
 exports.NODE_ENV = process.env.NODE_ENV || 'development';
+exports.MONGO_URI = process.env.MONGO_URI || null;
